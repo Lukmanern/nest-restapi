c58ca7c5f62df27a9f7d061521953002529b2a4c
LukmanE22 <unsurlukman@gmail.com>
2023-02-27 09:49:31 +0700

    update swagger-file-builder

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,17 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('SimpleTestCase example')
+    .setDescription('This is Desc')
     .setVersion('1.0')
-    .addTag('cats')
+    //.addTag('SimpleTestCase')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   await app.listen(3000);
-  app.enableCors();
+
   console.log('Application is running on: http://localhost:3000');
 }
 bootstrap();
